6b8cf9a0b78cb6836f8dbbd40263de2e76a48024
Mintyy999 <84873886+Mintyy999@users.noreply.github.com>
2021-05-26 16:23:29 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
 var Coins = 1;
 var CPS = 0;
+var CPC = 1;
 
 //first clicker (0.1 coins per second)
 var copyerPrice = 1.00 * (1.05 * copyerOwned)
 var copyerOwned = 0
+
+
+function myFunction() {
+  var Coins = var Coins + 1
+}
